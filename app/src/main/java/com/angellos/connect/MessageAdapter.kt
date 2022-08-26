1cb234df145a1b5d24274fa87f229bad02bf0c64
Angel1os <101555514+Angel1os@users.noreply.github.com>
2022-08-26 13:09:00 +0000

    Created a the chat screen activity and mesage model and adapter

---
+++ app/src/main/java/com/angellos/connect/MessageAdapter.kt
@@ -0,0 +1,27 @@
+package com.angellos.connect
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MessageAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    class SentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    class ReceiveViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
